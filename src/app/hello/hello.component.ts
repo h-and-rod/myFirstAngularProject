ed1b1cdbf9dbd2fd68ee52356d616d7080fe4b5c
Henrique Andrade <h.andrade.rodri@gmail.com>
2025-02-18 10:31:07 -0300

    feat: add Angular component for user profile

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  standalone: false,
+  templateUrl: './hello.component.html',
+  styleUrl: './hello.component.css'
+})
+export class HelloComponent {
+
+}
